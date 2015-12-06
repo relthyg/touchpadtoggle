56c1d918ed77c7a8d68ebf0be61ba35f3500bdbc
relthyg <relthyg@posteo.de>
2015-12-06 12:41:52 +0100

    Initial commit

---
+++ touchpadtoggle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Filename: /opt/bin/touchpadtoggle.sh
+#
+# Is executed by /etc/udev/rules.d//70-touchpad.rules.
+# Toggles touchpad and sends message via notify-send.
+
+if [ $1 -eq 0 ]
+then
+	MOUSE="Mouse detached"
+	PAD="Touchpad turned on"
+else
+	MOUSE="Mouse attached"
+	PAD="Touchpad turned off"
+fi
+
+USERS=$(users | sort | tr " " "\n" | sort -u | tr "\n" " ")
+
+for USER in $USERS; do
+	su $USER -c "XAUTHORITY=/home/icke/.Xauthority DISPLAY=:0 notify-send \"$MOUSE\" \"$PAD\""
+	su $USER -c "XAUTHORITY=/home/icke/.Xauthority DISPLAY=:0 synclient touchpadoff=$1"
+done
